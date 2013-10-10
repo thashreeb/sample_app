028bc3bba339ba28e0ba186babf120c95ec21ffe
Thashreeb <thashreeb@gmail.com>
2013-10-10 09:18:14 +0530

    Add user microposts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '991adce61d5c4afafc152ef391b28d3acd8a12f3c325a52c703568296290337ebe30ff0da4ab0888c1a5440286b43937e3018ff3e62b108a9d80472715057072'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	subject { page }
+
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1',		text: heading) }
+		it { should have_selector('title', 	text: full_title(page_title)) }
+	end
+
+	describe "Home page" do
+	  	before { visit root_path }
+
+		let(:heading)	{ 'Sample App' }
+		let(:page_title) { '' }
+
+		it_should_behave_like "all static pages"
+		it { should_not have_selector 'title', text: '| Home' }
+
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
+		end
+	end
+
+	describe "Help page" do
+		before { visit help_path }
+
+		let(:help)	{ 'Help' }
+		let(:page_title)	{ '' }
+	end
+
+	describe "About page" do
+		before { visit about_path }
+
+		let(:help)	{ 'About' }
+		let(:page_title)	{ '' }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		let(:help)	{ 'Contact' }
+		let(:page_title)	{ '' }
+	end
+end
